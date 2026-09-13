6aacfd10ee1855ada59c31de78db4bb4f10fc39e
Shivam <shivamgarade05@gmail.com>
2026-09-13 22:37:35 +0530

    Add initial Express server setup with notes API endpoints
    - Created package.json for project dependencies
    - Implemented server.js with CRUD operations for notes:
      - POST /notes to add a note
      - GET /notes to retrieve all notes
      - DELETE /notes/:index to delete a note by index
      - PATCH /notes/:index to update a note's title by index

---
+++ Backend/04_project_1/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json()); // by default express not read req.body
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
+  res.json({
+    message: "Notes added successfully!",
+    notes,
+  });
+});
+
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  delete notes[index];
+  res.json({
+    message: "note deleted successfully",
+  });
+});
+
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const { title } = req.body;
+  console.log(title)
+  notes[index].title = title;
+  res.json({
+    message: "note deleted successfully",
+    updatedNote : notes[index]
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server is runnning...");
+});
