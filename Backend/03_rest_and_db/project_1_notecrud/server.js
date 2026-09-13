221c4ba7010e07b6bf7cd38f80506b74202ff896
Shivam <shivamgarade05@gmail.com>
2026-09-13 19:52:13 +0530

    feat: initialize note CRUD project with Express server and basic POST endpoint

---
+++ Backend/03_rest_and_db/project_1_notecrud/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+
+const app = express()
+app.use(express.json())// build in middleware
+
+const notes = []
+
+app.post('/notes',(req,res)=>{
+    console.log(req.body)
+    notes.push(req.body)
+    res.json({
+        message:"notes added successfully",
+        notes:notes
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('Server is running...')
+}) 
