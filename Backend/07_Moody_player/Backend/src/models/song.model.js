448b9b5082d7916c7a5c8f539858ed99e24e3dff
Shivam <shivamgarade05@gmail.com>
2026-09-19 22:14:04 +0530

    feat: add song model and API routes for song upload and retrieval

--- Backend/07_Moody_player/Backend/src/models/song.model.js
+++ Backend/07_Moody_player/Backend/src/models/song.model.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const songSchema = new mongoose.Schema({
     title: String,
     artist: String,
-    audio: String
+    audio: String,
+    mood:String,
+    url:String
 });
 
 const song = mongoose.model('song', songSchema);
--- Backend/07_Moody_player/Backend/src/routes/song.routes.js
+++ Backend/07_Moody_player/Backend/src/routes/song.routes.js
@@ -2,23 +2,40 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const { uploadFile } = require('../services/storage.service');
+const Song = require('../models/song.model');
 
 // its for file handling , we store file in ram temperary
 const upload = multer({storage:multer.memoryStorage()})
 
 // song upload , create in db.
 router.post("/songs",upload.single("audio"), async (req, res) => {
-    console.log(req.body)
-    console.log(req.file)
 
     const fileData = await uploadFile(req.file)
-    console.log(fileData)
+    
+    // saving song in db
+    const song = await Song.create({
+        title:req.body.title,
+        artist:req.body.artist,
+        mood:req.body.mood,
+        url:fileData.url
+    })
+
     res.json({
         message:"Song created successfully.",
-        song:{
-           data:req.body,
-           file:req.file
-        }
+        song:song
+    })
+})
+
+// get method for getting songs
+router.get("/songs",async(req,res)=>{
+    const {mood} = req.query
+    
+    const songs = await Song.find({
+        mood:mood
+    })
+
+    res.status(200).json({
+        songs:songs
     })
 })
 
